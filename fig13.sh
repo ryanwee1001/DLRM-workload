e947808a0f4b7aaf6a2f029a4f00c2446c0d9089
Ryan Wee <rw2800@columbia.edu>
2023-10-26 23:40:10 -0500

    Update logging filepaths in test scripts

--- fig13.sh
+++ fig13.sh
@@ -45,118 +45,131 @@ PyGetHT='import sys; c=int(sys.argv[1]); print(",".join(str(2*i + off) for off i
 sed -i '441s/, auto/) # auto/1' \
         $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py
 
-NUM_BATCH=120   # Change this to modify runtime and max RSS
+NUM_BATCH=10000 # Change this to modify runtime and max RSS 
 BS=64
-LOG=results/fig13-$(date +%m%d)-$(date +%H%M%S).log
+RESULTS_DIR=results
+RESULTS_NAME=$(date +%m%d)-$(date +%H%M%S)
 INSTANCES=1
 EXTRA_FLAGS=
 GDB='gdb --args'
 DLRM_SYSTEMS=$DLRM_SYSTEM
 
+mkdir -p $RESULTS_DIR
+
 # RM2_1, low
 BOT_MLP=256-128-128
 TOP_MLP=128-64-1
 EMBS='128,1000000,60,120'
+TEST_NAME=fig13_RM2_1_low
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_low/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM2_2,low
 BOT_MLP=1024-512-128-128
 TOP_MLP=384-192-1
 EMBS='128,1000000,120,150'
+TEST_NAME=fig13_RM2_2_low
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_low/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
-done
-
-# RM2_3,low
-BOT_MLP=2048-1024-256-128
-TOP_MLP=512-256-1
-EMBS='128,1000000,170,180'
-for e in $EMBS; do
-    IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
-    EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
-    DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_low/table_1M.txt,$EMB_ROW"
-    C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM2_1, med
 BOT_MLP=256-128-128
 TOP_MLP=128-64-1
 EMBS='128,1000000,60,120'
+TEST_NAME=fig13_RM2_1_med
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_medium/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM2_2,med
 BOT_MLP=1024-512-128-128
 TOP_MLP=384-192-1
 EMBS='128,1000000,120,150'
+TEST_NAME=fig13_RM2_2_med
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_medium/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
-# RM2_3,med
-BOT_MLP=2048-1024-256-128
-TOP_MLP=512-256-1
-EMBS='128,1000000,170,180'
-for e in $EMBS; do
-    IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
-    EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
-    DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_medium/table_1M.txt,$EMB_ROW"
-    C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
-done
-
-##RM2_1, high
+# RM2_1, high
 BOT_MLP=256-128-128
 TOP_MLP=128-64-1
 EMBS='128,1000000,60,120'
+TEST_NAME=fig13_RM2_1_high
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_high/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM2_2,high
 BOT_MLP=1024-512-128-128
 TOP_MLP=384-192-1
 EMBS='128,1000000,120,150'
+TEST_NAME=fig13_RM2_2_high
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_high/table_1M.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
+# The original script runs some additional tests using the RM2_3 model. We don't
+# run them because the memory needed to generate the embeddings alone is ~90 GB,
+# and our machine only has around 64 GB of memory anyways.
+
+# RM2_3,low
+# BOT_MLP=2048-1024-256-128
+# TOP_MLP=512-256-1
+# EMBS='128,1000000,170,180'
+# for e in $EMBS; do
+#     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
+#     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
+#     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_low/table_1M.txt,$EMB_ROW"
+#     C=$(python -c "$PyGetCore" "$INSTANCES")
+#     numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$RESULTS_NAME
+# done
+
+# RM2_3,med
+# BOT_MLP=2048-1024-256-128
+# TOP_MLP=512-256-1
+# EMBS='128,1000000,170,180'
+# for e in $EMBS; do
+#     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
+#     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
+#     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_medium/table_1M.txt,$EMB_ROW"
+#     C=$(python -c "$PyGetCore" "$INSTANCES")
+#     numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$RESULTS_NAME
+# done
+
 # RM2_3,high
-BOT_MLP=2048-1024-256-128
-TOP_MLP=512-256-1
-EMBS='128,1000000,170,180'
-for e in $EMBS; do
-    IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
-    EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
-    DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_high/table_1M.txt,$EMB_ROW"
-    C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
-done
+# BOT_MLP=2048-1024-256-128
+# TOP_MLP=512-256-1
+# EMBS='128,1000000,170,180'
+# for e in $EMBS; do
+#     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
+#     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
+#     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_high/table_1M.txt,$EMB_ROW"
+#     C=$(python -c "$PyGetCore" "$INSTANCES")
+#     numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$RESULTS_NAME
+# done
--- fig14.sh
+++ fig14.sh
@@ -45,47 +45,52 @@ PyGetHT='import sys; c=int(sys.argv[1]); print(",".join(str(2*i + off) for off i
 sed -i '441s/, auto/) # auto/1' \
         $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py
 
-NUM_BATCH=120   # Change this to modify runtime and max RSS
+NUM_BATCH=10000 # Change this to modify runtime and max RSS
 BS=64
-LOG=results/fig14-$(date +%m%d)-$(date +%H%M%S).log
+RESULTS_DIR=results
+RESULTS_NAME=$(date +%m%d)-$(date +%H%M%S)
 INSTANCES=1
 EXTRA_FLAGS=
 GDB='gdb --args'
 DLRM_SYSTEMS=$DLRM_SYSTEM
 
+mkdir -p $RESULTS_DIR
+
 # RM1, low
 TOP_MLP=768-384-1
 BOT_MLP=2048-2048-256-64
 EMBS='64,500000,32,80'
+TEST_NAME=fig14_RM1_low
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_low/table_500K.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM1, med
 TOP_MLP=768-384-1
 BOT_MLP=2048-2048-256-64
 EMBS='64,500000,32,80'
+TEST_NAME=fig14_RM1_med
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_medium/table_500K.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
 
 # RM1, high
 TOP_MLP=768-384-1
 BOT_MLP=2048-2048-256-64
 EMBS='64,500000,32,80'
+TEST_NAME=fig14_RM1_high
 for e in $EMBS; do
     IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_high/table_500K.txt,$EMB_ROW"
     C=$(python -c "$PyGetCore" "$INSTANCES")
-    numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $LOG
+    /usr/bin/time -vo $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.time numactl -C $C -m 0 $CONDA_PREFIX/bin/python -u $MODELS_PATH/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 --numpy-rand-seed=727 --ipex-interaction --inference-only --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=10 --print-time --mini-batch-size=$BS --share-weight-instance=$INSTANCES $EXTRA_FLAGS | tee -a $RESULTS_DIR/$TEST_NAME.$RESULTS_NAME.log
 done
-
